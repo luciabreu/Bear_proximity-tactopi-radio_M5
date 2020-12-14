96ff7ffdc8113ff048695fa77c4256480ac8b4b3
Lúcia Abreu <26051893+luciabreu@users.noreply.github.com>
2020-12-14 23:47:11 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,31 @@
-radio.onReceivedValue(function (name, value) {
+radio.onReceivedNumber(function (receivedNumber) {
     signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
-    music.setTempo(Math.map(signal, -98, -42, 75, 300))
-    if (signal == -42) {
-        music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
+    if (!(playing)) {
+        if (signal >= -50) {
+            music.stopMelody(MelodyStopOptions.All)
+            music.playMelody("C D C C - D - C5 ", 355)
+            playing = true
+        } else {
+            music.setTempo(Math.map(signal, -128, -42, 20, 300))
+        }
     }
 })
+input.onButtonPressed(Button.A, function () {
+    startMusic()
+})
+music.onEvent(MusicEvent.MelodyEnded, function () {
+    playing = false
+})
+input.onButtonPressed(Button.B, function () {
+    startMusic()
+})
+function startMusic () {
+    music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Forever)
+    music.setTempo(40)
+}
 let signal = 0
-basic.showIcon(IconNames.Ghost)
+let playing = false
+basic.showIcon(IconNames.Heart)
 radio.setGroup(31)
-music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Forever)
-music.setTempo(100)
+playing = false
+startMusic()
